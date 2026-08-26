9572777a5e119cad6f85c79035494d73786a04cc
jjkeko12 <loberad1111@gmail.com>
2026-08-25 21:40:03 -0600

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,14 @@
+self.addEventListener('push', function (event) {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || 'Notificación';
+  const options = {
+    body: data.body || '',
+    tag: 'aura-global',
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
 
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
